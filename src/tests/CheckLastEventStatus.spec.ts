d3d79b5905bfac4eb4ab97ad7dacf8b713d63e02
blvkroot <hsalukamba17@gmail.com>
2021-12-14 21:58:44 +0100

    LoadLastEventRepository Interface created to decouple dependency

--- src/tests/CheckLastEventStatus.spec.ts
+++ src/tests/CheckLastEventStatus.spec.ts
@@ -1,18 +1,26 @@
 class CheckLastEventStatus {
 
-    constructor(private readonly loadLastEventRepository: LoadLastEventRepository) {}
+    constructor(private readonly loadLastEventRepository: ILoadLastEventRepository) {}
     async execute(group_id: string): Promise<void> {
-
+        await this.loadLastEventRepository.loadLastEvent(group_id);
     }
 }
 
-class LoadLastEventRepository {
+interface ILoadLastEventRepository {
+    loadLastEvent(group_id: string): Promise<void>;
+}
+
+class LoadLastEventRepositoryMock implements ILoadLastEventRepository{
     group_id?: string;
+
+    async loadLastEvent(group_id: string): Promise<void> {
+        this.group_id = group_id;
+    }
 }
 
 describe("CheckLastEventStatus", () => {
     it("should get last event data", async () => {
-        const loadLastEventRepository = new LoadLastEventRepository();
+        const loadLastEventRepository = new LoadLastEventRepositoryMock();
         const checkLastEventStatus = new CheckLastEventStatus(loadLastEventRepository);
 
         await checkLastEventStatus.execute('any_group_id');
