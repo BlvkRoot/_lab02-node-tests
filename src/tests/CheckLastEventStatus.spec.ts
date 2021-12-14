55275ef96bf4bd97f0ea40de70a8a878851729d0
blvkroot <hsalukamba17@gmail.com>
2021-12-14 22:07:20 +0100

    CheckLastEventStatus ensure calls loadLastEvent only once

--- src/tests/CheckLastEventStatus.spec.ts
+++ src/tests/CheckLastEventStatus.spec.ts
@@ -3,6 +3,7 @@ class CheckLastEventStatus {
     constructor(private readonly loadLastEventRepository: ILoadLastEventRepository) {}
     async execute(group_id: string): Promise<void> {
         await this.loadLastEventRepository.loadLastEvent(group_id);
+        await this.loadLastEventRepository.loadLastEvent(group_id);
     }
 }
 
@@ -12,9 +13,11 @@ interface ILoadLastEventRepository {
 
 class LoadLastEventRepositoryMock implements ILoadLastEventRepository{
     group_id?: string;
+    callsCount = 0
 
     async loadLastEvent(group_id: string): Promise<void> {
         this.group_id = group_id;
+        this.callsCount++;
     }
 }
 
@@ -26,5 +29,6 @@ describe("CheckLastEventStatus", () => {
         await checkLastEventStatus.execute('any_group_id');
 
         expect(loadLastEventRepository.group_id).toBe('any_group_id');
+        expect(loadLastEventRepository.callsCount).toBe(1);
     })
 });
