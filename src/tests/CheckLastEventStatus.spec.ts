ae2976f0bdedcdc3d23db88a7c212e0865dcb8be
blvkroot <hsalukamba17@gmail.com>
2021-12-14 21:28:10 +0100

    first commit

---
+++ src/tests/CheckLastEventStatus.spec.ts
@@ -0,0 +1,20 @@
+class CheckLastEventStatus {
+    async execute(group_id: string): Promise<void> {
+
+    }
+}
+
+class LoadLastEventRepository {
+    group_id?: string;
+}
+
+describe("CheckLastEventStatus", () => {
+    it("should get last event data", async () => {
+        const loadLastEventRepository = new LoadLastEventRepository();
+        const checkLastEventStatus = new CheckLastEventStatus();
+
+        await checkLastEventStatus.execute('any_group_id');
+
+        expect(loadLastEventRepository.group_id).toBe('any_group_id');
+    })
+});
